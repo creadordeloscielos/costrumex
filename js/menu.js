e3bbe76beeff98aa0a6ae8b396a828d2d71588cc
salvador galvan hernadez <vida.suero.oral.18@gmail.com>
2021-06-21 22:58:32 -0500

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,24 @@
 let btn = document.getElementById("btn");
 let menu = document.getElementById("menu");
-let close = document.getElementById("close");
-contador = 0;
+let contador = 0;
 
-btn.addEventListener("click", function(){
 
-    if (contador == 0){
-        menu.className =("mostrar");
-        contador = 1;
-    }else {
-        menu.classList.remove("mostrar");
-        contador = 0;
-    }
+btn.addEventListener('click', function(){
+
+ if (contador == 0){
+     menu.className=("mostrar");
+     contador=1;
+ } else {
+     menu.classList.remove("mostrar");
+     contador=0;
+ }
+
+
 })
 
-window.addEventListener ("resize", function(){
-    if(screen.width > 750){
-        menu.classList.remove("mostrar");
-        contador = 0;
-    }
+window.addEventListener("resize", function(){
+   if(screen.width > 730){
+       menu.classList.remove("mostrar");
+       contador = 0;
+   }
 })
